bf79bed4056256b74a00c602774b70e3454fdffd
jakiasultana2022 <rokeyasbd@gmail.com>
2023-05-19 21:36:43 +0600

    second page

--- src/components/SecondPage.tsx
+++ src/components/SecondPage.tsx
@@ -1,11 +1,38 @@
+import { useEffect, useState } from 'react';
+import { DataGrid, GridColumns } from '@mui/x-data-grid';
 
+interface Post {
+  id: number;
+  title: string;
+  body: string;
+}
 
 const SecondPage = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const [posts, setPosts] = useState<Post[]>([]);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then((response) => response.json())
+      .then((data) => setPosts(data));
+  }, []);
+
+  const columns: GridColumns = [
+    { field: 'id', headerName: 'ID', width: 90 },
+    { field: 'title', headerName: 'Title', width: 200 },
+    { field: 'body', headerName: 'Body', width: 400 },
+  ];
+
+  return (
+    <div style={{
+        position: 'absolute', 
+        left: '50%', 
+        top: '60%',
+        transform: 'translate(-50%, -30%)'
+    }}>
+     <DataGrid rows={posts} columns={columns} />
+ 
+    </div>
+  );
 };
 
 export default SecondPage;
