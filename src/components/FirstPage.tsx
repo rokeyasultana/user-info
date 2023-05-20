72a33639c84745d706ef50d27c80401f4740ac3a
jakiasultana2022 <rokeyasbd@gmail.com>
2023-05-20 21:22:30 +0600

    form validation

--- src/components/FirstPage.tsx
+++ src/components/FirstPage.tsx
@@ -1,76 +1,93 @@
-import React, { useState } from 'react';
-import { Box, Button, Container, TextField, Typography } from '@mui/material';
+import React from 'react';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import { Button, Container, TextField, Typography  } from '@mui/material';
+
+//validation schema
+const validationSchema = Yup.object({
+  name: Yup.string().required('Name is required'),
+  phoneNumber: Yup.string()
+  .matches(/^[0-9]+$/, 'Invalid Phone Number')
+  .min(10, 'Phone number must be at least 10 digits')
+  .max(15, 'Phone number can have maximum 15 digits')
+  .required('Phone number is required'),
+  email: Yup.string().email('Invalid Email').required('Email is required'),
+});
 
 const FirstPage = () => {
-  const [name, setName] = useState('');
-  const [phoneNumber, setPhoneNumber] = useState('');
-  const [email, setEmail] = useState('');
-
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-    const userDetails = {
-      name,
-      phoneNumber,
-      email,
-    };
-    localStorage.setItem('userDetails', JSON.stringify(userDetails));
-    // Redirect to the second page
-    window.location.href = '/second-page';
-  };
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      phoneNumber: '',
+      email: '',
+    },
+    validationSchema: validationSchema,
+    onSubmit: (values) => {
+      localStorage.setItem('userDetails', JSON.stringify(values));
+      // Redirect to the second page
+      window.location.href = '/second-page';
+    },
+  });
 
   return (
-
     <div style={{
       position: 'absolute', 
       left: '50%', 
       top: '20%',
-      transform: 'translate()'
+      transform: 'translate(-50%, -30%)'
   }}>
- <Container maxWidth="sm">
-      <Box
-        sx={{
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          mt: 8,
-        }}
-      >
-        <Typography variant="h4" component="h1" sx={{ mb: 4 }}>
-          Welcome!
+
+    <Container maxWidth="sm">
+    <Typography  variant="h4" component="h1" sx={{ mb: 4,color: 'success.blue', }}>
+          Welcome! <br></br>
+          Enter Your Details
         </Typography>
-        <form onSubmit={handleSubmit} style={{ width: '100%' }}>
-          <TextField
-            label="Name"
-            variant="outlined"
-            fullWidth
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            sx={{ mb: 2 }}
-          />
-          <TextField
-            label="Phone Number"
-            variant="outlined"
-            fullWidth
-            value={phoneNumber}
-            onChange={(e) => setPhoneNumber(e.target.value)}
-            sx={{ mb: 2 }}
-          />
-          <TextField
-            label="Email"
-            variant="outlined"
-            fullWidth
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            sx={{ mb: 2 }}
-          />
-          <Button type="submit" variant="contained" size="large">
-            Submit
-          </Button>
-        </form>
-      </Box>
+      <form onSubmit={formik.handleSubmit}>
+        <TextField
+          id="name"
+          name="name"
+          label="Name"
+          variant="outlined"
+          fullWidth
+          value={formik.values.name}
+          onChange={formik.handleChange}
+          error={formik.touched.name && Boolean(formik.errors.name)}
+          helperText={formik.touched.name && formik.errors.name}
+          sx={{ mb: 2 }}
+        />
+
+        <TextField
+          id="phoneNumber"
+          name="phoneNumber"
+          label="Phone Number"
+          variant="outlined"
+          fullWidth
+          value={formik.values.phoneNumber}
+          onChange={formik.handleChange}
+          error={formik.touched.phoneNumber && Boolean(formik.errors.phoneNumber)}
+          helperText={formik.touched.phoneNumber && formik.errors.phoneNumber}
+          sx={{ mb: 2 }}
+       />
+
+        <TextField
+          id="email"
+          name="email"
+          label="Email"
+          variant="outlined"
+          fullWidth
+          value={formik.values.email}
+          onChange={formik.handleChange}
+          error={formik.touched.email && Boolean(formik.errors.email)}
+          helperText={formik.touched.email && formik.errors.email}
+          sx={{ mb: 2 }}
+       />
+
+        <Button type="submit" variant="contained" color="primary">
+          Submit
+        </Button>
+      </form>
     </Container>
     </div>
-   
   );
 };
 
